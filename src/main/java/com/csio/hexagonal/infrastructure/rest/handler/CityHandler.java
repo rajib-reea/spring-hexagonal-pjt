9b9a87571a348d353bfa866804cb9abf7cf603f7
Md. Rajib Hossain Pavel <rajib.pavel@erainfotechbd.com>
2025-12-29 12:03:03 +0600

    running swagger without example body.

--- src/main/java/com/csio/hexagonal/infrastructure/rest/handler/CityHandler.java
+++ src/main/java/com/csio/hexagonal/infrastructure/rest/handler/CityHandler.java
@@ -1,8 +1,52 @@
+// package com.csio.hexagonal.infrastructure.rest.handler;
+
+// import com.csio.hexagonal.application.port.in.CommandUseCase;
+// import com.csio.hexagonal.domain.model.City;
+// import com.csio.hexagonal.infrastructure.rest.mapper.CityMapper;
+// import com.csio.hexagonal.infrastructure.rest.request.CreateCityRequest;
+// import com.csio.hexagonal.infrastructure.rest.response.city.CityResponse;
+// import com.csio.hexagonal.infrastructure.rest.spec.CitySpec;
+// import io.swagger.v3.oas.annotations.Operation;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.reactive.function.server.ServerRequest;
+// import org.springframework.web.reactive.function.server.ServerResponse;
+// import reactor.core.publisher.Mono;
+
+// @Component
+// public class CityHandler {
+
+//     private final CommandUseCase<City, CityResponse> commandUseCase;
+//     private final CityMapper mapper;
+//      private final String entityName = "City";
+
+
+//     public CityHandler(
+//             CommandUseCase<City, CityResponse> commandUseCase,
+//             CityMapper mapper
+//     ) {
+//         this.commandUseCase = commandUseCase;
+//         this.mapper = mapper;
+//     }
+
+//     @Operation(
+//         summary = CitySpec.CREATE_SUMMARY,
+//         description = CitySpec.CREATE_DESCRIPTION
+//     )
+//     public Mono<ServerResponse> createCity(ServerRequest request) {
+
+//         String token = request.headers()
+//                               .firstHeader("Authorization");
+
+//         return request.bodyToMono(CreateCityRequest.class)
+//                 .map(mapper::toModel)
+//                 .map(model -> commandUseCase.create(model, token))
+//                 .flatMap(result -> ServerResponse.ok().bodyValue(result));
+//     }
+// }
 package com.csio.hexagonal.infrastructure.rest.handler;
 
 import com.csio.hexagonal.application.port.in.CommandUseCase;
-import com.csio.hexagonal.domain.model.City;
-import com.csio.hexagonal.infrastructure.rest.mapper.CityMapper;
+import com.csio.hexagonal.application.usecase.CreateCityCommand;
 import com.csio.hexagonal.infrastructure.rest.request.CreateCityRequest;
 import com.csio.hexagonal.infrastructure.rest.response.city.CityResponse;
 import com.csio.hexagonal.infrastructure.rest.spec.CitySpec;
@@ -15,17 +59,12 @@ import reactor.core.publisher.Mono;
 @Component
 public class CityHandler {
 
-    private final CommandUseCase<City, CityResponse> commandUseCase;
-    private final CityMapper mapper;
-     private final String entityName = "City";
-
+    private final CommandUseCase<CreateCityCommand, CityResponse> commandUseCase;
 
     public CityHandler(
-            CommandUseCase<City, CityResponse> commandUseCase,
-            CityMapper mapper
+            CommandUseCase<CreateCityCommand, CityResponse> commandUseCase
     ) {
         this.commandUseCase = commandUseCase;
-        this.mapper = mapper;
     }
 
     @Operation(
@@ -34,12 +73,11 @@ public class CityHandler {
     )
     public Mono<ServerResponse> createCity(ServerRequest request) {
 
-        String token = request.headers()
-                              .firstHeader("Authorization");
+        String token = request.headers().firstHeader("Authorization");
 
         return request.bodyToMono(CreateCityRequest.class)
-                .map(mapper::toModel)
-                .map(model -> commandUseCase.create(model, token))
-                .flatMap(result -> ServerResponse.ok().bodyValue(result));
+            .map(req -> new CreateCityCommand(req.name(), req.state()))
+            .map(cmd -> commandUseCase.create(cmd, token))
+            .flatMap(res -> ServerResponse.ok().bodyValue(res));
     }
 }

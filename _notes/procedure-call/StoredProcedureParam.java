442118c73343aa018659e91018fec193c16f22d7
Md. Rajib Hossain Pavel <135492408+rajib-reea@users.noreply.github.com>
2026-01-18 13:58:11 +0600

    Create StoredProcedureParam.java

---
+++ _notes/procedure-call/StoredProcedureParam.java
@@ -0,0 +1,25 @@
+package com.csio.hexagonal.infrastructure.store.persistence.adapter;
+
+import javax.persistence.ParameterMode;
+
+/**
+ * Interface that enums should implement to describe a stored-procedure parameter.
+ * Implement one enum per procedure (or per logical group).
+ */
+public interface StoredProcedureParam {
+    /**
+     * The parameter name as the stored procedure expects it (case and naming depends on DB).
+     */
+    String name();
+
+    /**
+     * The JPA ParameterMode (IN, OUT, INOUT, REF_CURSOR).
+     */
+    ParameterMode mode();
+
+    /**
+     * Java type of the parameter. For REF_CURSOR use Void.class if you will read result list,
+     * or the entity/DTO class if you want JPA to map the result set to that class.
+     */
+    Class<?> type();
+}

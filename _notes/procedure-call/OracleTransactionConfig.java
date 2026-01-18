4432d3cb5ddc23ed4ba4417a465755e67291acf9
Md. Rajib Hossain Pavel <135492408+rajib-reea@users.noreply.github.com>
2026-01-18 13:34:28 +0600

    Create OracleTransactionConfig.java

---
+++ _notes/procedure-call/OracleTransactionConfig.java
@@ -0,0 +1,21 @@
+package com.csio.hexagonal.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+public class OracleTransactionConfig {
+
+    @Bean(name = "oracleTransactionManager")
+    public PlatformTransactionManager oracleTransactionManager(
+            @Qualifier("oracleEntityManagerFactory") EntityManagerFactory oracleEntityManagerFactory) {
+        return new JpaTransactionManager(oracleEntityManagerFactory);
+    }
+}
